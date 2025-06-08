4832871f992c7a03a96ef94bec324d7797c795a5
Caine Phung <hphung3324@conestogac.on.ca>
2025-06-08 10:52:14 -0400

    Clean snapshot after fixing read-tree cleanup

--- ugit/base.py
+++ ugit/base.py
@@ -48,13 +48,33 @@ def get_tree (oid, base_path=''):
             assert False, f'Unknown tree entry {type_}'
     return result
 
+def _empty_current_directory ():
+    for root, dirnames, filenames in os.walk ('.', topdown=False):
+        for filename in filenames:
+            path = os.path.relpath (f'{root}/{filename}')
+            if is_ignored (path) or not os.path.isfile (path):
+                continue
+            os.remove (path)
+        for dirname in dirnames:
+            path = os.path.relpath (f'{root}/{dirname}')
+            if is_ignored (path):
+                continue
+            try:
+                os.rmdir (path)
+            except (FileNotFoundError, OSError):
+                # Deletion might fail if the directory contains ignored files,
+                # so it's OK
+                pass
 
 def read_tree (tree_oid):
+    _empty_current_directory ()
     for path, oid in get_tree (tree_oid, base_path='./').items ():
         os.makedirs (os.path.dirname (path), exist_ok=True)
         with open (path, 'wb') as f:
             f.write (data.get_object (oid))
 
 
-def is_ignored (path):
-    return '.ugit' in path.split ('/')
+def is_ignored(path):
+    parts = path.split(os.sep)
+    return '.ugit' in parts or '.git' in parts
+
